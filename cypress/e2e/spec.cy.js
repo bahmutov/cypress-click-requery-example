b2497a066c87ef5d6dd909f0e61cea9c1b8ea508
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-26 17:04:50 -0500

    working

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,19 +2,45 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="cypress" />
 
+const currentSubject = cy.currentSubject.bind(cy)
+cy.currentSubject = (chainerId) => {
+  const subject = currentSubject(chainerId)
+  if (
+    subject &&
+    Cypress.dom.isElement(subject) &&
+    Cypress.dom.isDetached(subject)
+  ) {
+    console.warn('detached element', subject)
+    // replace the subject with new DOM element with the same ID
+    // or CSS selector if found
+    const doc = cy.state('document')
+    const newElement = doc.getElementById(
+      subject.attr('id'),
+    )
+    if (newElement) {
+      console.warn(
+        'replaced with current element',
+        newElement,
+      )
+      const newSubject = Cypress.$(newElement)
+      cy.state('subjects')[chainerId] = newSubject
+
+      return newSubject
+    }
+  }
+  return subject
+}
+
 it('clicks on the Go button', () => {
   cy.visit('index.html')
   cy.contains('button', 'Go').click()
   cy.contains('output', 'Gone')
 })
 
-it.only('clicks on the Ready button', () => {
+it('clicks on the Ready button', () => {
   cy.visit('index.html')
   cy.contains('button', 'Ready').click()
   cy.contains('output', 'Ready')
-  cy.contains('button', 'Go')
-    .should('be.visible')
-    .wait(1000)
-    .click()
+  cy.contains('button', 'Go').wait(1000).click()
   cy.contains('output', 'Gone')
 })
